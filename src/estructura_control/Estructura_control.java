0f81231e963669a7190826b917bbc7ec5196f9c8
GodsM4ster <edwinmarinballes@hotmail.com>
2022-08-18 17:59:26 -0500

    agregando el proyecto

---
+++ src/estructura_control/Estructura_control.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package estructura_control;
+
+public class Estructura_control {
+
+    public static void main(String[] args) {
+        
+        boolean valor_log = true;
+        boolean valor_log2 = false;
+        boolean valor_log3 = true;
+        
+        System.out.println("inicio del programa");
+                
+        if (valor_log == false){
+            System.out.println("this is truth");
+        } else if(valor_log2 == true){
+            System.out.println("maybe this is good");
+        } else if( (valor_log3 == false) && (valor_log2 ==false) ) {
+            System.out.println("no hubo de otra");
+        } else {
+            System.out.println("we are dumber");
+        }
+        
+        System.out.println("this is the end");
+    }
+    
+}
